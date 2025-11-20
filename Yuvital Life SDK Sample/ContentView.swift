e51615530fcd57738b0b98718ff6a0bfa78015db
Naum Asafov <nauma@yuvital.com>
2025-11-20 10:44:11 +0200

    Add initial project structure for Yuvital Life SDK Sample

---
+++ Yuvital Life SDK Sample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yuvital Life SDK Sample
+//
+//  Created by Naum Asafov on 20/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Yuvital Life SDK Sample")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Yuvital Life SDK Sample/Yuvital_Life_SDK_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yuvital_Life_SDK_SampleApp.swift
+//  Yuvital Life SDK Sample
+//
+//  Created by Naum Asafov on 20/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Yuvital_Life_SDK_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
